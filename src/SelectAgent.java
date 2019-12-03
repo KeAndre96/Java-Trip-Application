0f1db813d43113159f228a3bfcdb039f367137f6
KeAndre96 <keandregraham@hotmail.com>
2019-12-03 15:26:47 -0500

    Update SelectAgent.java

--- src/SelectAgent.java
+++ src/SelectAgent.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class SelectAgent {
     protected JPanel mainPanel;
@@ -49,5 +52,61 @@ public class SelectAgent {
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
+    }
+}
+final class AgentOptions{
+    private static final Object syncLock = new Object();
+    private static volatile AgentOptions _singletonAgents;
+
+    private java.util.List<TravelAgent> OptionsList;
+
+    private AgentOptions(){
+        ArrayList<TravelAgent> placeHolder = new ArrayList<TravelAgent>();
+        placeHolder.add(new TravelAgent("AgentK", "4041234567", "agentk@gmail.com", 123568));
+        placeHolder.add(new TravelAgent("AgentJ", "4041635537", "agentj@gmail.com", 123168));
+        placeHolder.add(new TravelAgent("AgentM", "4041234567", "agentm@gmail.com", 123547));
+        placeHolder.add(new TravelAgent("AgentN", "4041234867", "agentn@gmail.com", 123588));
+        placeHolder.add(new TravelAgent("AgentO", "4041234167", "agento@gmail.com", 123538));
+        OptionsList = Collections.unmodifiableList(placeHolder);
+    }
+    public static List GetsOptions(){
+        if(_singletonAgents == null){
+            synchronized (syncLock){
+                if(_singletonAgents == null){
+                    _singletonAgents = new AgentOptions();
+                }
+            }
+        }
+        return _singletonAgents.OptionsList;
+    }
+}
+final class TravelerOptions{
+    private static final Object syncLock = new Object();
+    private static volatile TravelerOptions _singletonTraveler;
+
+    private List<Traveler> OptionsList;
+
+    private TravelerOptions(){
+        ArrayList<Traveler> placeHolder = new ArrayList<>();
+        placeHolder.add(new Traveler("Adam Smith", "4041234567", "adamsmith@gmail.com",
+                "6455 Old Avenue"));
+        placeHolder.add(new Traveler("John Smith", "4041234567", "adamsmith@gmail.com",
+                "6455 Old Avenue"));
+        placeHolder.add(new Traveler("Robb Smith", "4041234567", "adamsmith@gmail.com",
+                "6455 Old Avenue"));
+        placeHolder.add(new Traveler("Jake Smith", "4041234567", "adamsmith@gmail.com",
+                "6455 Old Avenue"));
+        placeHolder.add(new Traveler("Lucy Smith", "4041234567", "adamsmith@gmail.com",
+                "6455 Old Avenue"));
+    }
+    public static List GetsOptions(){
+        if(_singletonTraveler == null){
+            synchronized (syncLock){
+                if(_singletonTraveler == null){
+                    _singletonTraveler = new TravelerOptions();
+                }
+            }
+        }
+        return _singletonTraveler.OptionsList;
     }
 }
