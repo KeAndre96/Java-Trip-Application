186efbe6d394de93d9da5d6bce65cb3cb71e4869
Jopanzo <jpnzo2k12@gmail.com>
2019-12-08 12:45:07 -0500

    Add files via upload

--- src/trip.java
+++ src/trip.java
@@ -3,45 +3,64 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONString;
+import org.json.JSONWriter;
 import java.lang.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.IOException;
-import org.json.JSONWriter;
+
 /**
  *
  * @author johnn
  */
 public class trip {
-    JSONObject tripJson = new JSONObject();
-    JSONArray Travelers = new JSONArray();
+    JSONObject Trip = new JSONObject();
+    JSONObject Travelers = new JSONObject();
+    public int state;
+    JSONArray jArray;
     Scanner sc = new Scanner(System.in);
-    private String id;
-    private String thankYouNote;
-
-    public trip(){}
-    public void addPackages(int i)
-    {   //This requires the use of getting the int value of package
-        tripJson.put("packages", "Packages" + Integer.toString(i));
-    }
-    public void addTravelers(String name)
+    public trip()
     {
-        tripJson.put("travelers",name);
+        state = 1;
+    }
+    public void addPackages(String packageName)
+    {   //This requires the use of getting the int value of package
+
+        Trip.put("packages", "Packages " + packageName);
+        //System.out.println("Added Package.");
+    }
+    public void state(int i)
+    {
+        Trip.put("state",Integer.toString(i));
     }
     public void addAgent(String nameOfAgent)
     {
-        tripJson.put("agent", nameOfAgent);
+        Trip.put("agent", nameOfAgent);
+        state = 1;
+       // System.out.println("Added agent");
+    }
+    public void addTravelers(String name)
+    {
+        Travelers.put("traveler",name);
+     // System.out.println("Added Traveler");
+    }
+    public void addedTravelers()
+    {
+        jArray = new JSONArray();
+        jArray.put(Travelers);
+        Trip.put("travelers",jArray);
     }
     public void writeTrip()
     {
         try(FileWriter file = new FileWriter("trips.json"))
         {
-            file.write(tripJson.toString());
+            file.write(Trip.toString());
             file.flush();
         //Files.write(Paths.get("tripsFile"), Trip.toJSONString().getBytes());
         }
