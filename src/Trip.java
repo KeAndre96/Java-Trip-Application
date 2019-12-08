794cd47fc1bf5b1e5c4b4a02018801d9e2d35a34
Tanner Jones <tannercolejones@gmail.com>
2019-12-08 16:05:41 -0500

    Jopanzo and I were working on same task. Merged inputs.

--- src/Trip.java
+++ src/Trip.java
@@ -3,67 +3,58 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONString;
-import org.json.JSONWriter;
 import java.lang.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import org.json.JSONWriter;
 /**
  *
  * @author johnn
  */
-public class Trip   {
-    private JSONObject Trip = new JSONObject();
-    private JSONObject Travelers = new JSONObject();
-    private int state;
-    private TravelAgent agent;
+public class Trip {
+    JSONObject tripJson = new JSONObject();
+    JSONArray Travelers = new JSONArray();
     Scanner sc = new Scanner(System.in);
-    void trip()
-    {
-        state = 1;
-    }
-    public void addPackages(String packageName)
-    {   //This requires the use of getting the int value of package
+    private String id;
+    private String thankYouNote;
 
-        Trip.put("packages", "Packages " + packageName);
-        //System.out.println("Added Package.");
+    public Trip(){}
+    public void addPackages(String i)
+    {   //This requires the use of getting the int value of package
+        tripJson.put("packages", "Packages" + i);
+    }
+    public void addTravelers(Person traveler)
+    {
+        tripJson.put("travelers",traveler.getName()); //not sure if this is used
+        // System.out.println("Added Traveler");
     }
     public void setState(int i)
     {
-        Trip.put("setState",Integer.toString(i));
-        this.state = i;
+        tripJson.put("state",Integer.toString(i));
     }
-    public void addAgent(Person agent)
+    public void addAgent(String nameOfAgent)
     {
-        Trip.put("agent", agent.getName());
-        state = 1;
-       // System.out.println("Added agent");
+        tripJson.put("agent", nameOfAgent);
+        //state = 1;
+        // System.out.println("Added agent");
     }
-    public void addTravelers(String name)
-    {
-        Travelers.put("traveler",name);
-     // System.out.println("Added Traveler");
-    }
-    public void addedTravelers()
-    {
-        JSONArray jArray = new JSONArray();
-        jArray.put(Travelers);
-        Trip.put("travelers", jArray);
+    public void addAgent(Person person){
+        tripJson.put("agent", person.getName());
+
     }
     public void writeTrip()
     {
         try(FileWriter file = new FileWriter("trips.json"))
         {
-            file.write(Trip.toString());
+            file.write(tripJson.toString());
             file.flush();
-        //Files.write(Paths.get("tripsFile"), Trip.toJSONString().getBytes());
+            //Files.write(Paths.get("tripsFile"), Trip.toJSONString().getBytes());
         }
         catch(Exception e)
         {
